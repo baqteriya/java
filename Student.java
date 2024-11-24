7716714481a95fdc2666de7f92207aeda9c51e85
baqteriya <kumaririya1030@gmail.com>
2024-11-24 15:39:08 +0530

    student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+ class Student{
+    public static void main(String[] args) 
+    {
+System.out.println("Lnct university");
+System.out.println("name:moni singh");
+System.out.println("subject name science");
+int i=100;
+if(i>40)
+System.out.println("pass");
+else{
+    System.out.println("fail");
+}
+    }
+}
