d3644c1db31582b0f24ebcccc30d23f6ff67d1e1
baqteriya <kumaririya1030@gmail.com>
2024-11-24 16:14:36 +0530

    adder.java

---
+++ Adder.java
@@ -0,0 +1,27 @@
+  class Adder {
+        static int adder (int a,int b)
+     {
+            return a+b;
+     } 
+      
+
+      static int adder (int a,int b,int c)
+     {
+        return a+b+c;
+         }
+            
+            public static void main(String[] args) {
+                System.out.println("adder.add(11,11)");
+                System.out.println("adder.add(11,11,11)");
+            }
+         }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
