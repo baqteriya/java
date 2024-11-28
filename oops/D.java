2d41e3845cf5d7a9321a3a33f1b5c6b741a56f19
baqteriya <kumaririya1030@gmail.com>
2024-11-28 22:23:53 +0530

    create.java

---
+++ oops/D.java
@@ -0,0 +1,32 @@
+class D {
+    public static void main(String[] args) {
+        int n=4;
+        switch(n)
+        {
+            case 1:
+            System.out.print("sunday");
+            break;
+            case 2:
+            System.out.print("monday");
+            break;
+            case 3:
+            System.out.print("tuesday");
+            break;
+            case 4:
+            System.out.print("wednesday");
+            break;
+            case 5:
+            System.out.print("thursday");
+            break;
+            case 6:
+            System.out.print("friday");
+            break;
+            case 7:
+            System.out.print("saturday");
+            break;
+            default:
+            System.out.println("not present");
+        }
+    }
+    
+}
