e6ba6cad76f55d88bff0933facb1af02de3cb3bb
baqteriya <kumaririya1030@gmail.com>
2024-11-28 22:15:22 +0530

    create.java

---
+++ oops/C.java
@@ -0,0 +1,16 @@
+class C {
+    public static void main(String[] args) {
+        int number=20;
+        switch(number)
+        {
+            case 10:
+            System.out.println("no is 10");
+            break;
+            case 20:
+            System.out .print("no is 20");
+            default:
+            System.out.print("no is  present");
+    }
+    
+}
+}
