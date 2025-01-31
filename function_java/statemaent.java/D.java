1474e5585f783d1edcec776efb0098c1559cdf72
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-01-31 23:54:58 +0530

    Create D.java

---
+++ function_java/statemaent.java/D.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class D {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        {
+            int subject=sc.nextInt();
+            switch(subject)
+            {
+                case 1:
+                System.out.println("math");
+                break;
+                case 2: 
+                System.out.println("physics");
+                break;
+                case 3:
+                System.out.println("english");
+                break;
+                case 4:
+                System.out.println("pps");
+                break;
+                case 5:
+                System.out.println("TW");
+                break;
+                case 6:
+                System.out.println("invalid subject");
+                break;
+                }
+
+
+            }
+        }
+    }
