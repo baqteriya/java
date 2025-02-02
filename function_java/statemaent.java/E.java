a88ade792e45f52de35856aaa59dc5e9318085f5
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-02-03 00:14:07 +0530

    Create E.java

---
+++ function_java/statemaent.java/E.java
@@ -0,0 +1,21 @@
+
+ public class E{
+    public static void main(String[] args) {
+        System.out.println("subject:physics");
+        System.out.println("name:moni");
+        System.out.println("rollno:121");
+        System.out.println("enrollno:LNCDBTC23456");
+        System.out.println("ECLS");
+            int i=100;
+            if(i>40){
+                System.out.println("pass");
+            }
+                    else
+                    {
+                System.out.println("fail");
+                }
+            }
+        }
+    
+    
+
