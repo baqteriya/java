8f696af36689937b2255fc4299d4aa3f340fb019
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-02-04 23:56:59 +0530

    Create B.java

---
+++ function_java/statemaent.java/string/B.java
@@ -0,0 +1,9 @@
+ class B {//create class
+public static void main(String args[]) {//main method
+    //concatenation
+    String firstName="riya";
+    String fullName="firstName+lastName";
+    System.out.println(fullName);
+    System.out.println(fullName.length());
+}
+}
