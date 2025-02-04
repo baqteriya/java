c11f11caf8fa68dfc0ff0116dcd4cc3e92af8652
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-02-05 00:02:39 +0530

    Create A.java

---
+++ function_java/statemaent.java/string/instance variable/A.java
@@ -0,0 +1,12 @@
+
+import  java.util.*;
+public class A {
+    public static void main(String[] args) //main method
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=5;//local variable
+        int b=20;
+        int Sum=a + b;
+        System.out.println("Sum: "+Sum);
+    }
+}
