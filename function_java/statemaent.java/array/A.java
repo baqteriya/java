9f0893cdea2acced82f6646e77027351a1a28175
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-02-05 23:03:04 +0530

    Create A.java

---
+++ function_java/statemaent.java/array/A.java
@@ -0,0 +1,17 @@
+
+
+public class A {
+    public static void main(String[] args) {
+        int[]subject=new int[5];
+        subject [0]=45;//eng
+        subject [1]=47;//math
+        subject[2]=56;//hindi
+        subject[3]=89;//phy
+        subject[4]=54;//pps
+        for( int i=0;i< subject.length;i++)
+        {
+            System.out.println("subject"+(i+1)+"marks:"+subject[i]);
+        }
+    }
+    
+}
