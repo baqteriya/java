f13fb146cc10ab818e0c1ce7281520bfeebffb21
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-03-19 22:31:30 +0530

    Create Function3.java

---
+++ function_java/Function3.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class Function3 {
+    public static int Add(int a,int b) {
+        int sum =a+b;
+        return sum;
+        
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=Add(a,b);
+        System.out.println(sum);
+
+        }
+    }
