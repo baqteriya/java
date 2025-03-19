7ae7cad24a8cc8bb5c302beb442f54b91736f689
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-03-19 22:15:46 +0530

    Create Function2.java

---
+++ function_java/Function2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Function2 {
+    public static void MyName(String name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        MyName(name);//call kiya function ko
+        
+    }
+    
+}
