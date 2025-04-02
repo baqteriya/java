e724357eb3f28d0256f3f9ecb20f99cd72fa24f3
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-04-02 23:15:39 +0530

    Create A.java

---
+++ .vscode/A.java
@@ -0,0 +1,19 @@
+
+class A {
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+        int sum = (a + b);
+        int multi = (a * b);
+        int division = (a / b);
+
+        System.out.println(sum);
+        {
+            System.out.println(multi);
+            {
+                System.out.println(division);
+            }
+        }
+    }
+}
