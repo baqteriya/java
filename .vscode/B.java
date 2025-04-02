10c450109bef01dacec76a4af004d4d908d99cdd
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-04-03 00:07:10 +0530

    Create B.java

---
+++ .vscode/B.java
@@ -0,0 +1,14 @@
+
+class B {
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 50;
+        {
+            for (int i = 0; i <= a; i++) {
+                System.out.println(i + " ");
+
+            }
+        }
+    }
+}
