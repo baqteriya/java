37a782f02fc9809705d43a3fdb5269639581e2d1
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-04-08 23:01:29 +0530

    Create Inheritance.java

---
+++ oops/Inheritance.java
@@ -0,0 +1,17 @@
+
+class Shapes {
+
+    String color;
+}
+
+class Triangle extends Shapes {
+
+}
+
+class Inheritance {
+
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        t1.color = "red";
+    }
+}
