d6a21a9132bd4df18ec5463d61acbaf7a0869220
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-04-25 00:07:37 +0530

    Create Linkedlist.java

---
+++ List/Linkedlist.java
@@ -0,0 +1,67 @@
+
+class Linkedlist {
+
+    Node head;
+
+    class Node {
+
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    //add first and last
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+
+        }
+        currNode.next = newNode;
+    }
+
+    //print
+    public void printList() {
+        if (head == null) {
+            System.out.println("list is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.println(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        list.addLast("List");
+        list.printList();
+        list.addFirst("this");
+        list.printList();
+    }
+}
