09fd2725a605d2a457c343f86da0fa33ce20afff
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 15:33:01 +0530

    Create E.java

---
+++ .vscode/Basicprogram/E.java
@@ -0,0 +1,12 @@
+
+class E {
+
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 2;
+        int c = a > b ? a : b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
