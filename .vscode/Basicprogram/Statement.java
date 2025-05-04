c0e6f86efba92cff369c003af99b324283071a33
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 16:02:15 +0530

    Create Statement.java

---
+++ .vscode/Basicprogram/Statement.java
@@ -0,0 +1,11 @@
+
+class Statement {
+
+    public static void main(String[] args) {
+        if (10 > 20) {
+            System.out.println("Hello");
+        } else {
+            System.out.println("Hii");
+        }
+    }
+}
