4877121b3bfde2ee44d8a93c71c464b8d7269616
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 15:48:45 +0530

    Create H.java

---
+++ .vscode/Basicprogram/H.java
@@ -0,0 +1,12 @@
+
+class H {
+
+    public static void main(String[] args) {
+        int a = 65;
+        int b = 15;
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(~a);
+    }
+}
