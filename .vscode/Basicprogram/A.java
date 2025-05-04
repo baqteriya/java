f5e1a83fad65318ae606398a7d836287ed0c943d
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 15:08:44 +0530

    Create A.java

---
+++ .vscode/Basicprogram/A.java
@@ -0,0 +1,11 @@
+
+class A {
+
+    public static void main(String[] args) {
+        int a = 9;
+        int b = a++ + ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
