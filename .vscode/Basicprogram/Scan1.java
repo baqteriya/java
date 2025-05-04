8474b0d617f0d26d078eb681eab6cf41d7cd1b50
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 17:13:52 +0530

    Create Scan1.java

---
+++ .vscode/Basicprogram/Scan1.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class Scan1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();//Mohni Sharma
+        String b = sc.nextLine();//Moni Shirivastava
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
