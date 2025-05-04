b8a94e09c4810c0f87ded95b920b266a98bb4945
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 15:17:26 +0530

    Create C.java

---
+++ .vscode/Basicprogram/C.java
@@ -0,0 +1,11 @@
+
+class C {
+
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 2;
+        System.out.println(a > b && b < a);
+        System.out.println(b >= a && b != a);
+        System.out.println(b >= a || b != a);
+    }
+}
