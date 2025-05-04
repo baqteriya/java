47a170712fbfbffd5c33d4feae0a5ba7f13b1bdd
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 16:37:20 +0530

    Create Statement3.java

---
+++ .vscode/Basicprogram/Statement3.java
@@ -0,0 +1,22 @@
+
+class Statement3 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        final int b = 20;
+        switch (a) {
+            case 10:
+                System.out.println("Hello");
+                break;
+            case 20:
+                System.out.println("Hii");
+                break;
+            case 30:
+                System.out.println("Bye");
+                break;
+            default:
+                System.out.println("Good");
+
+        }
+    }
+}
