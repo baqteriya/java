d3a9954ccc867b3ee5f4b1d661594456e1a32446
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 17:22:44 +0530

    Create Whileloop1.java

---
+++ .vscode/Basicprogram/Whileloop1.java
@@ -0,0 +1,12 @@
+
+class Whileloop1 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        while (a >= 1) {
+            System.out.println(a);
+            a--;
+        }
+
+    }
+}
