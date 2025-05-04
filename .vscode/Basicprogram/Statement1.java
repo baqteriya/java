1ca0f49c83b489e29f765b44a38acdc68bacecea
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-05-04 16:12:36 +0530

    Create Statement1.java

---
+++ .vscode/Basicprogram/Statement1.java
@@ -0,0 +1,17 @@
+
+class Statement1 {
+
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 50;
+        int c = 20;
+
+        if (a < b) {
+            System.out.println("Hello");
+        } else if (c < b) {
+            System.out.println("Hii");
+        } else {
+            System.out.println("helloworld");
+        }
+    }
+}
