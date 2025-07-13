dbf08af17141f38d38e94e1f859e234661382592
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-07-13 20:27:51 +0530

    used of nextLine

---
+++ Statement/C.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+class C {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        sc.nextLine();
+        String b = sc.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
