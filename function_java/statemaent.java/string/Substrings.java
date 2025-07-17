d720ced9f978285eeace75c5f66b5e78cb5e3786
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-07-17 16:19:05 +0530

    substrings

---
+++ function_java/statemaent.java/string/Substrings.java
@@ -0,0 +1,9 @@
+
+public class Substrings {
+
+    public static void main(String[] args) {
+        String sentence = "my name is Riya";
+        String name = sentence.substring(11);
+        System.out.println(name);
+    }
+}
