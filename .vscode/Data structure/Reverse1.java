53618059d727fc0bccdd31ea31a12d3cac65feb4
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-08-11 20:53:28 +0530

    Create Reverse1.java

---
+++ .vscode/Data structure/Reverse1.java
@@ -0,0 +1,11 @@
+
+public class Reverse1 {
+
+    public static void main(String[] args) {
+        int arr[] = {2, 5, 8, 3};
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            System.out.println(arr[i] + "");
+        }
+    }
+}
