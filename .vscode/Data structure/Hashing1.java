0ced07435a992942cde427d8cf28c04c0388e47d
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-08-17 22:32:42 +0530

    Create Hashing1.java

---
+++ .vscode/Data structure/Hashing1.java
@@ -0,0 +1,11 @@
+
+class Hashing1 {
+
+    public static void main(String[] args) {
+        int arr[] = {12, 15, 18};
+        for (int val : arr) {
+            System.out.println(val + "");
+        }
+
+    }
+}
