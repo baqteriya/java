c081f4b78d8bf9f7546e5f43d9f82f8535716908
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-08-24 23:17:58 +0530

    Create K.java

---
+++ .vscode/Basicprogram/K.java
@@ -0,0 +1,13 @@
+
+
+class K {
+    public static void main(String[] args) {
+        int age=19;
+        String name="Hello";
+        String city="jamui";
+        System.out.print(name+"Riya kumari\n");
+          System.out.print("city:"+city+"\n");
+            System.out.print("Age:"+age);
+        
+    }
+}
