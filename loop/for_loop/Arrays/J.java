1568cafa0e57d73bd73c46ff0bc9d4070db728f2
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-09-17 20:55:09 +0530

    Create J.java

---
+++ loop/for_loop/Arrays/J.java
@@ -0,0 +1,11 @@
+public class J {
+    public static void main(String[] args) {
+        int arr[] = {98, 89, 43, 23, 12};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum +arr[i];
+        }
+        System.out.println("sum of array element:"+sum);
+    }
+    
+}
