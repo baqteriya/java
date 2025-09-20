3d314d16eb32fb51e3c1a53217ba0a5bb1556dd5
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-09-20 12:55:52 +0530

    Create B.java

---
+++ loop/for_loop/Arrays/B.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class B {
+    public static void main(String[] args) {
+        int arr[]={12,11,34,23,45,22};
+        Arrays.sort(arr);
+        System.out.println("Array is Ascending order:");
+        for(int num:arr){
+            System.out.println(num+"");
+        }
+    }
+    
+}
