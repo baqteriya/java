0cb4974c1c909185a9e2c3c0290819803b5910b4
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-10-05 18:48:00 +0530

    Create Ab.java

---
+++ .vscode/Basicprogram/Ab.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Ab {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number 1:");
+        float a=sc.nextFloat();
+     System.out.println("Enter number 2:");
+     float b=sc.nextFloat();
+     float sum=a+b;
+     System.out.println("the sum of these number is");
+     System.out.println(sum);
+
+
+    }
+    
+}
