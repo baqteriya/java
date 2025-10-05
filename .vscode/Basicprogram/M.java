163bc495d4de516d1ae8774c3c00ea74015f23ca
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-10-05 18:09:17 +0530

    add 3 number

---
+++ .vscode/Basicprogram/M.java
@@ -0,0 +1,10 @@
+public class M {
+    public static void main(String[] args) {
+        int a=123;
+        int b=677;
+        int c=122;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+    
+}
