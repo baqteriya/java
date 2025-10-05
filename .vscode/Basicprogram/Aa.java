54af0c453afbba8911368db04de18f845a92540b
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-10-05 18:56:41 +0530

    Create Aa.java

---
+++ .vscode/Basicprogram/Aa.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Aa {
+    public static void main(String[] args) {
+        System.out.println("Taking input From the user");
+        Scanner sc=new Scanner(System.in);
+        boolean b1=sc.hasNextInt();
+        System.out.println(b1);
+    }
+    
+}
