4f88fd83c70fecc344629a55684fb660e7c6c119
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-10-08 00:40:57 +0530

    Create Af.java

---
+++ .vscode/Basicprogram/Af.java
@@ -0,0 +1,14 @@
+public class Af {
+    public static void main(String[] args) {
+       int x=6;
+       int y=1;
+        int k=x*y/2;
+        System.out.println(k);
+        int b=1;
+        int c=4;
+        int a=5;
+        x=b*b-(4*a*c)/(2*a);
+        System.out.println(x);
+    }
+    
+}
