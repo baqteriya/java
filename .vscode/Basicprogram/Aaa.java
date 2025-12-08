a44d2f18e4b8c8f6a0c2298737f75853b8939383
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-12-08 21:12:29 +0530

    Create Aaa.java

---
+++ .vscode/Basicprogram/Aaa.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Aaa {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the  Yourname");
+        String name=sc.nextLine();
+        System.out.println("Enter the name"+name);
+        System.out.println("Enter your lucky number");
+        int num_1=sc.nextInt();
+        System.out.println("Enter lucky number is"+num_1);
+    }
+    
+}
