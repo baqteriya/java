819dd015035bf1010fc3037b94b4cb59f13c6954
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-12-12 00:11:41 +0530

    Create Agg.java

---
+++ .vscode/Basicprogram/Agg.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Agg {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+                int n=sc.nextInt();
+        if(n>99 && n<1000){
+            System.out.println("This is a 3 digit number");
+        }else{
+            System.out.println("It is not a 3 digit number");
+        }
+        
+    }
+    
+}
