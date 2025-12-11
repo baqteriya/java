ef442d303b74a3b076846bd17fb0656a5c8b79bf
baqteriya <167676465+baqteriya@users.noreply.github.com>
2025-12-12 00:00:22 +0530

    Create Aff.java

---
+++ .vscode/Basicprogram/Aff.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Aff {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter percentage");
+        int n=sc.nextInt();
+        if(n>90){
+            System.out.println("Excellent");
+        }else if (n>80) {
+            System.out.println("Very Good");
+            
+        }else if (n>70) {
+            System.out.println("Good");
+            
+        }
+        else if (n>60) {
+            System.out.println("Can do better");
+            
+        }else if(n>50){
+            System.out.println("Average");
+
+        }else if(n>40){
+            System.out.println("Below Average");
+        }
+        else{
+            System.out.println("Fail");
+        }
+
+        
+    }
+    
+}
