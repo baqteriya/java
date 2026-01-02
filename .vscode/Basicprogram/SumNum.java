7ea190ef1db63005d8c9bf3c0c432c3b37b39cb0
baqteriya <167676465+baqteriya@users.noreply.github.com>
2026-01-02 18:34:24 +0530

    Create SumNum.java

---
+++ .vscode/Basicprogram/SumNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumNum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int num = 1;
+        while(num <= n){
+            sum = sum + num ;
+            num = num + 1;
+        }
+          System.out.println(sum);
+    }
+    
+}
